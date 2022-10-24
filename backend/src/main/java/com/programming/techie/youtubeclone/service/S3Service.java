31588b24a0c9901b69f29d87250f36e2fdaf0e65
chunkityip <alex0917lfo@hotmail.com>
2022-10-24 17:32:58 -0400

    Adding aws info to application.properties

--- backend/src/main/java/com/programming/techie/youtubeclone/service/S3Service.java
+++ backend/src/main/java/com/programming/techie/youtubeclone/service/S3Service.java
@@ -1,5 +1,6 @@
 package com.programming.techie.youtubeclone.service;
 
+import com.amazonaws.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 public class S3Service implements FileService {
@@ -7,6 +8,9 @@ public class S3Service implements FileService {
     @Override
     public String uploadFile(MultipartFile file) {
         //Upload file to AWS
+
+        //Prepare a Key
+        var filenameExtension = StringUtils.getFilenameExtension(file.getOriginalFilename());
         return null;
     }
 }
